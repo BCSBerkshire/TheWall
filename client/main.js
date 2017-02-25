b11afbd29b613f0dabb653a19ac8fbf88e109b82
Florian Rathgeber <florian.rathgeber@gmail.com>
2017-02-25 15:46:07 +0000

    Refactor, load materialize from CDN

--- client/main.js
+++ client/main.js
@@ -3,20 +3,15 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.start.helpers({
+  title: 'The Wall',
+  tagline: 'Build em up to smash em down'
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+(function($){
+  $(function(){
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
